a839ccad4930ed22407249942539f2883a2ce3df
Ferran Xarnach <flixil@gmail.com>
2011-04-21 14:40:23 +0200

    Project Papereria Clip initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PapereriaClip::Application.config.secret_token = '141c29ba4b8f25528c99f35c39f6d5df9b20fed884d9f339037d8c72350bbc9a23cf47fe5b71bdfb1a50ebda389bfc108c2abcd2984a2a2504a594ac11be5509'
